ac68eec9b02f0bf9d8e7455e397095f407f4aef0
Gabriel <gabrielhenrique.ls96@gmail.com>
2021-02-01 14:46:22 -0300

    Refactor do component router-link

--- src/app/router-link/router-link.component.ts
+++ src/app/router-link/router-link.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class RouterLinkComponent implements OnInit {
 
   @Input() title: string
+  
+  @Input() path: string
 
   constructor() { }
 
